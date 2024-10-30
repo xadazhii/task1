ce46afefc5e9d3ef189b76e9cce60d1f07cc9e9e
Kristina Adazhii <xadazhii@stuba.sk>
2024-10-30 18:17:06 +0100

    Update

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,16 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Scanner sc = new Scanner(System.in);
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        System.out.println("Enter the studentID, name, surname, age: ");
+        Student student = new Student(sc.nextLine(), sc.nextLine(), sc.nextLine(), sc.nextInt());
+        System.out.println(student.getInfo());
+
+        System.out.println("Enter the name, surname, age: ");
+        Employer employer = new Employer(sc.next(), sc.next(), sc.nextInt());
+        System.out.println(employer.getInfo());
+
     }
 }
