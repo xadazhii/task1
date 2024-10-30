72e3112957f6c9a5f800212497942e0fb080e826
Kristina Adazhii <xadazhii@stuba.sk>
2024-10-30 18:17:39 +0100

    Update

--- src/Student.java
+++ src/Student.java
@@ -1,2 +1,19 @@
-package PACKAGE_NAME;public class Student {
+public class Student implements Person{
+    private String studentID;
+    private String name;
+    private String surname;
+    private int age;
+
+    public Student(String studentID, String name, String surname, int age) {
+        this.studentID = studentID;
+        this.name = name;
+        this.surname = surname;
+        this.age = age;
+    }
+
+    @Override
+    public String getInfo(){
+            return "studentID: " + studentID + ", name: " + name + ", surname: " + surname + ", age: " + age;
+    }
+
 }
