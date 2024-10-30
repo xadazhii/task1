6687e5369e0eec5d246b9b45628a87263b0e3cb3
Kristina Adazhii <xadazhii@stuba.sk>
2024-10-30 18:16:40 +0100

    Update Employer.java

--- src/Employer.java
+++ src/Employer.java
@@ -1,2 +1,17 @@
-package PACKAGE_NAME;public class Employer {
+public class Employer implements Person{
+    private String name;
+    private String surname;
+    private int age;
+
+    public Employer(String name, String surname, int age) {
+        this.name = name;
+        this.surname = surname;
+        this.age = age;
+    }
+
+    @Override
+    public String getInfo(){
+        return "Name: " + name + " Surname: " + surname + " Age: " + age;
+    }
+
 }
